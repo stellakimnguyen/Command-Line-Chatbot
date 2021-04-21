5d4a882e823cceb8a6a78e83d6fd77ece8aee58e
Stella Nguyen <stellakimnguyen@gmail.com>
2021-04-20 21:23:47 -0400

    Add SOEN 321 hierarchy and loop through folders

--- test.py
+++ test.py
@@ -3,15 +3,35 @@ import requests
 from tika import parser
 import spotlight
 import json
+import os
 
-parsed = parser.from_file('universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.pdf')
-# url = "https://api.dbpedia-spotlight.org/en/annotate?text="
-# print(url)
-# req = requests.get(url)
-annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate?text=', parsed["content"])
-# annotations = spotlight.annotate('http://localhost/rest/annotate', parsed["content"], confidence=0.4, support=20)
 
-# print(parsed["metadata"])
-# print(parsed["content"])
-File_object = open("universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.json", "w+")
-File_object.write(json.dumps(annotations))
+def list_files(localdir):
+    r = []
+    subdirs = [x[0] for x in os.walk(localdir)]
+    for subdir in subdirs:
+        files = os.walk(subdir).__next__()[0]
+        if (len(files) > 0):
+            for file in files:
+                r.append(os.path.join(subdir, file))
+    return r
+
+
+# parsed = parser.from_file('universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.pdf')
+# annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate?text=', parsed["content"])
+
+# Loop 1: Course
+
+# Loop 2: Week/Outline
+# Loop 3: Lecture/Labs/Tutorials
+# Loop 4: Content (Slides/Worksheets/Instructions)
+
+# File_object = open("universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.json", "w+")
+# File_object.write(json.dumps(annotations))
+
+def main():
+    print(list_files('universities\\Concordia University'))
+
+
+if __name__ == "__main__":
+    main()
