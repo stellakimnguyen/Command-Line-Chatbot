76a084f8679a85c3797218acacbb0e08035902ab
Stella Nguyen <stellakimnguyen@gmail.com>
2021-04-20 21:59:25 -0400

    Parse all documents into txt and entity linking with DB Spotlight

--- entityLinking.py
+++ entityLinking.py
@@ -1,35 +1,30 @@
-import tika
-import requests
 from tika import parser
 import spotlight
 import json
 import os
 
 
-def list_files(localdir):
-    r = []
+def parse_all_documents(localdir):
+    # going through subfolders
     subdirs = [x[0] for x in os.walk(localdir)]
     for subdir in subdirs:
         files = os.walk(subdir).__next__()[2]
         if (len(files) > 0):
             for file in files:
-                r.append(os.path.join(subdir, file))
+                # parse pdf documents into txt
                 parsed = parser.from_file(os.path.join(subdir, file))
+                # calling spotlight api
                 annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate?text=',
                                                  parsed["content"])
-                File_object = open(os.path.join(subdir, file).replace('pdf', 'json'),
+                # create json file
+                file_object = open(os.path.join(subdir, file).replace('pdf', 'json'),
                                    "w+")
-                File_object.write(json.dumps(annotations))
-    return r
+                # write spotlight response to json file
+                file_object.write(json.dumps(annotations))
 
-# Loop 1: Course
-
-# Loop 2: Week/Outline
-# Loop 3: Lecture/Labs/Tutorials
-# Loop 4: Content (Slides/Worksheets/Instructions)
 
 def main():
-    print(list_files('universities\\Concordia University'))
+    parse_all_documents('universities\\Concordia University')
 
 
 if __name__ == "__main__":
