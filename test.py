b830cdf50bc6b79655f5d57d92a7b334a2ef3c64
Stella Nguyen <stellakimnguyen@gmail.com>
2021-04-20 20:49:26 -0400

    Add tika, spotlight and automatic URI test file

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import tika
+import requests
 from tika import parser
 import spotlight
+import json
 
 parsed = parser.from_file('universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.pdf')
-annotations = spotlight.annotate('http://spotlight.dbpedia.org/rest/', parsed["content"], confidence=0.4, support=20)
+# url = "https://api.dbpedia-spotlight.org/en/annotate?text="
+# print(url)
+# req = requests.get(url)
+annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate?text=', parsed["content"])
+# annotations = spotlight.annotate('http://localhost/rest/annotate', parsed["content"], confidence=0.4, support=20)
 
 # print(parsed["metadata"])
 # print(parsed["content"])
-# File_object = open("slides1.txt", "w+")
-# File_object.write(parsed["content"])
+File_object = open("universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.json", "w+")
+File_object.write(json.dumps(annotations))
