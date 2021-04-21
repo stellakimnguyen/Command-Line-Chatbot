f10d123c2dac0435cfcbdf29618b85042c60aefa
Stella Nguyen <stellakimnguyen@gmail.com>
2021-04-20 21:45:38 -0400

    Add SOEN 321 files

--- test.py
+++ test.py
@@ -10,24 +10,23 @@ def list_files(localdir):
     r = []
     subdirs = [x[0] for x in os.walk(localdir)]
     for subdir in subdirs:
-        files = os.walk(subdir).__next__()[1]
+        files = os.walk(subdir).__next__()[2]
         if (len(files) > 0):
             for file in files:
                 r.append(os.path.join(subdir, file))
+                parsed = parser.from_file(os.path.join(subdir, file))
+                annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate?text=',
+                                                 parsed["content"])
+                File_object = open(os.path.join(subdir, file).replace('pdf', 'json'),
+                                   "w+")
+                File_object.write(json.dumps(annotations))
     return r
-
-
-# parsed = parser.from_file('universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.pdf')
-# annotations = spotlight.annotate('https://api.dbpedia-spotlight.org/en/annotate?text=', parsed["content"])
 
 # Loop 1: Course
 
 # Loop 2: Week/Outline
 # Loop 3: Lecture/Labs/Tutorials
 # Loop 4: Content (Slides/Worksheets/Instructions)
-
-# File_object = open("universities/Concordia University/COMP474/Week 1/Lectures/Slides/slides01.json", "w+")
-# File_object.write(json.dumps(annotations))
 
 def main():
     print(list_files('universities\\Concordia University'))
